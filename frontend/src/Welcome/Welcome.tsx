175c0f9cdf870a76dd1ada4ffb366b2c752c5d4d
koki1019 <lukaodzelashvili@gmail.com>
2023-06-28 11:55:51 +0100

    Fonts & Welcome Page

---
+++ frontend/src/Welcome/Welcome.tsx
@@ -0,0 +1,26 @@
+import "./Welcome.css";
+
+function Welcome() {
+	return (
+		<>
+			<nav></nav>
+			<div>
+				<header>
+					<h1>Beezle</h1>
+					<h3>
+						An
+						upcoming
+						social
+						media
+						platform
+					</h3>
+				</header>
+				<div className="other-side">
+					<form className="welcome-form-registration"></form>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default Welcome;
