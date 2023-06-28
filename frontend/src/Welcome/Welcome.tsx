e82775da0750105d92339554b0ec07f80e402591
koki1019 <lukaodzelashvili@gmail.com>
2023-06-28 12:30:13 +0100

    Input form css (not done)

--- frontend/src/Welcome/Welcome.tsx
+++ frontend/src/Welcome/Welcome.tsx
@@ -18,7 +18,40 @@ function Welcome() {
 					</h3>
 				</header>
 				<div className="other-side">
-					<form className="welcome-form-registration"></form>
+					<form className="welcome-form-registration form-group">
+						<input
+							placeholder="Username"
+							className="form-control"
+						/>
+						<p>
+							Only
+							letters,
+							numbers,
+							dots,
+							and
+							underscores
+							are
+							allowed.
+						</p>
+
+						<input
+							placeholder="E-Mail Address"
+							className="form-control"
+						/>
+						<input
+							placeholder="Password"
+							className="form-control"
+							type="password"
+						/>
+						<input
+							placeholder="Confirm Password"
+							className="form-control"
+							type="password"
+						/>
+						<button type="submit">
+							REGISTER
+						</button>
+					</form>
 				</div>
 			</div>
 		</>
