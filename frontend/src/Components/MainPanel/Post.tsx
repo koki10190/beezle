ce7f3074ecb7b2f0ab6c5a1ccdf652f65ccbf6c7
koki1019 <lukaodzelashvili@gmail.com>
2023-06-29 10:47:29 +0100

    Emoji Picker

--- frontend/src/Components/MainPanel/Post.tsx
+++ frontend/src/Components/MainPanel/Post.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Post.css";
 import UserType from "../../interfaces/UserType";
 import GetUserData from "../../api/GetUserData";
 import PostBox from "./PostBox";
+import EmojiPicker, { EmojiClickData, Theme } from "emoji-picker-react";
 
 function Post() {
 	let user: UserType;
 	const avatar = useRef<HTMLDivElement>(null);
 	const username = useRef<HTMLParagraphElement>(null);
 	const handle = useRef<HTMLParagraphElement>(null);
+	const post = useRef<HTMLTextAreaElement>(null);
+	const emojiPicker = useRef<HTMLDivElement>(null);
+
+	const [isEmojiPickerShown, setEmojiShown] = useState(false);
 
 	useEffect(() => {
 		(async () => {
@@ -17,12 +22,20 @@ function Post() {
 			avatar.current!.style.backgroundImage = `url("${user.avatar}")`;
 			username.current!.innerText = user.displayName;
 			handle.current!.innerText = "@" + user.handle;
+
+			post.current!.innerText = "";
 		})();
 	}, []);
 
 	const redirectToUserProfile = () => {
 		window.location.href = "/profile/" + user.handle;
 	};
+
+	const addEmoji = (emoji: EmojiClickData) => {
+		post.current!.value += emoji.emoji;
+	};
+
+	const showEmojiPicker = () => setEmojiShown(!isEmojiPickerShown);
 
 	return (
 		<div className="navigation-panel make-post">
@@ -47,10 +60,42 @@ function Post() {
 					@handle
 				</p>
 			</div>
-			<textarea
-				placeholder="Lorem ipsum dolor sit amet."
-				className="post-textarea"
-			></textarea>
+			<div className="post-text-box">
+				<textarea
+					ref={post}
+					placeholder="Lorem ipsum dolor sit amet."
+					className="post-textarea"
+				></textarea>
+				<div className="post-text-buttons">
+					<a
+						onClick={
+							showEmojiPicker
+						}
+					>
+						<i className="fa-solid fa-face-awesome"></i>
+					</a>
+					<button>Post</button>
+				</div>
+				{isEmojiPickerShown ? (
+					<div
+						className="emoji-picker-post"
+						ref={
+							emojiPicker
+						}
+					>
+						<EmojiPicker
+							onEmojiClick={
+								addEmoji
+							}
+							theme={
+								Theme.DARK
+							}
+						/>
+					</div>
+				) : (
+					<div></div>
+				)}
+			</div>
 			<hr
 				style={{ boxSizing: "content-box" }}
 				className="small-bar"
